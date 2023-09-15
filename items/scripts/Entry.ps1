f164d7feeb4cfebb06ea8a2cf897063f572359b3
Artsem Prashkovich <apr.dev@gmail.com>
2023-09-15 16:23:44 +0200

    Sync Items entrypoint is updated

--- items/scripts/Entry.ps1
+++ items/scripts/Entry.ps1
@@ -31,12 +31,11 @@ function Wait-Url ($url) {
     }
 }
 
-function SyncContent($idHost, $cmHost, $clientSecret, $clientName, $nugetSource, $userName, $password) {    
+function SyncContent($idHost, $cmHost, $clientSecret, $clientName) {    
     try {
         # Add nuget source & install Sitecore CLI
         Write-Host "Installing Sitecore CLI"
 
-        #dotnet nuget add source $nugetSource --name "Velux jFrog" --username $userName --password $password
         
         dotnet nuget add source https://sitecore.myget.org/F/sc-packages/api/v3/index.json --name "Sitecore-Public-Nuget-Feed"
         dotnet tool install -g sitecore.cli --version 5.1.25 --add-source https://sitecore.myget.org/F/sc-packages/api/v3/index.json
@@ -80,17 +79,16 @@ if (Wait-Url $cmHost.Value) {
     $idHost = Get-ChildItem -Path Env:\ID_HOST;    
     $clientSecret = Get-ChildItem -Path Env:\ID_CLIENT_SECRET;
     $clientName = Get-ChildItem -Path Env:\ID_CLIENT_NAME;
-    $nugetSource = Get-ChildItem -Path Env:\NUGET_SOURCE;
-    $userName = Get-ChildItem -Path Env:\JFROG_USERNAME;    
-    $password = Get-ChildItem -Path Env:\JFROG_PASSWORD;
 
-    SyncContent $idHost.Value $cmHost.Value $clientSecret.Value $clientName.Value $nugetSource.Value $userName.Value $password.Value
+    SyncContent $idHost.Value $cmHost.Value $clientSecret.Value $clientName.Value
 
     Write-Output "Serialization is done."
 
-    while ($true) {
-        Start-Sleep -s 600
-    }
+    #while ($true) {
+    #    Start-Sleep -s 600
+    #}
+	
+	Start-Sleep -s 120
 }
 else {
     Write-Output "Something goes wrong."
